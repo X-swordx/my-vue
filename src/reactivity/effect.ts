3f0f322f7e2e85ddc9e19dcb990f813ed0e8bbdf
zhengchengxuan <534370078@qq.com>
2022-06-14 23:15:37 +0800

    实现 effect 的 scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,7 +1,7 @@
 let activeEffect;
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn
   }
   run() {
@@ -12,6 +12,7 @@ class ReactiveEffect {
 
 const targetMap = new Map()
 export function track(target, key) {
+  // target -> key -> dep
   let depsMap = targetMap.get(target)
   if(!depsMap) {
     targetMap.set(target, depsMap = new Map())
@@ -28,12 +29,16 @@ export function trigger(target, key) {
   let dep = depsMap.get(key)
 
   for (const effect of dep) {
-    effect.run()
+    if(effect.scheduler) {
+      effect.scheduler()
+    }else {
+      effect.run()
+    }
   }
 }
 
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn, option: any = {}) {
+  const _effect = new ReactiveEffect(fn, option.scheduler)
 
   _effect.run()
 
--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -29,4 +29,34 @@ describe("effect", () => {
     const r = runner()
     expect(r).toBe('foo')
   })
+
+  it('scheduler', () => {
+    // 1. 通过 effect 第二个参数传入 一个 scheduler 的 fn
+    // 2. effect 第一次执行的时候 还会执行 effect回调函数
+    // 3. 但响应式数据 更新 的时候不会执行 effect回调函数 , 而是执行 scheduler
+    // 4. 如果说当执行 runner 的时候，会再次的执行 effect回调函数
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    })
+    const obj = reactive({
+      foo: 1
+    });
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      }, 
+      { scheduler }
+    )
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // 更新
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(dummy).toBe(1)
+    // 再次运行 runner，effect fn 也会再次执行
+    run()
+    expect(dummy).toBe(2)
+  })
 })
