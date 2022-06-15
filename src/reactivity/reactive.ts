3d2683ce10208b81e5e2db97bc04f004a1a3a674
zhengchengxuan <534370078@qq.com>
2022-06-16 00:11:25 +0800

    实现 readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,21 +1,15 @@
-import { track, trigger } from './effect'
+import { mutableHandlers, readonlyHandlers } from "./baseHandlers"
 
-export function reactive (raw) {
-  return new Proxy(raw , {
-    get(target, key) {
-      const res = Reflect.get(target, key)
 
-      // 依赖收集
-      track(target, key)
-      return res
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
 
-      // 触发依赖
-      trigger(target, key)
-      return res
-    }
-
-  })
+export function reactive(raw) {
+  return createActiveObject(raw, mutableHandlers)
 }
+
+export function readonly(raw) {
+  return createActiveObject(raw, readonlyHandlers)
+}
+function createActiveObject(raw: any, baseHandlers) {
+  return new Proxy(raw, baseHandlers)
+}
+
