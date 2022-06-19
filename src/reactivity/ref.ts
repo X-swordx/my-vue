23cf57498f21459d7c535676a7ea95539a7c8640
zhengchengxuan <534370078@qq.com>
2022-06-19 12:37:52 +0800

    实现 isRef 和 unRef

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any;
   public dep;
   private _rawValue: any;
+  public __v_isRef = true
   constructor(value) {
     this._rawValue = value
     this._value = convert(value)
@@ -37,5 +38,13 @@ function convert(value: any) {
 
 export const ref = (value) => {
   return new RefImpl(value)
+}
+
+export const isRef = (ref) => {
+  return !!ref.__v_isRef
+}
+
+export const unRef = (ref) => {
+  return isRef(ref) ? ref.value : ref
 }
 
