d068c2220fc5b96deae84067377b408dcf33fdf6
zhengchengxuan <534370078@qq.com>
2022-07-03 11:53:08 +0800

    实现解析 element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content)
   return createRoot(parseChildren(context))
@@ -7,13 +12,16 @@ export function baseParse(content: string) {
 
 function parseChildren(context) {
   const nodes: object[] = []
-
-  let node
-  if (context.source.startsWith("{{")) {
+  let node;
+  const s = context.source
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context)
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
   nodes.push(node)
-
   return nodes
 }
 
@@ -39,6 +47,28 @@ function parseInterpolation(context) {
   }
 }
 
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start)
+  parseTag(context, TagType.End)
+
+  return element
+}
+
+function parseTag(context: any, type: TagType) {
+  // <div></div>
+  const match: RegExpExecArray | null = /^<\/?([a-z]*)/i.exec(context.source)
+  // console.log('match ', match)
+  const tag = match![1]
+
+  advanceBy(context, match![0].length + 1)
+
+  if(type === TagType.End) return
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
+}
+
 function createRoot(children) {
   return {
     children,
@@ -52,6 +82,6 @@ function createParserContext(content: string): any {
 }
 
 // 推进：删除已经处理好的模板
-function advanceBy(context, length) {
+function advanceBy(context: any, length: number) {
   context.source = context.source.slice(length);
 }
