969c85f37e857588ce0e1184057fd1dc4dd6b115
zhengchengxuan <534370078@qq.com>
2023-02-09 22:56:41 +0800

    fix: packages路径匹配问题

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,13 @@
-import { defineConfig } from "vitest/config";
-import path from 'path'
+/*
+ * @Author: zhengchengxuan 534370078@qq.com
+ * @Date: 2023-01-11 21:12:08
+ * @LastEditors: zhengchengxuan 534370078@qq.com
+ * @LastEditTime: 2023-02-09 22:52:24
+ * @FilePath: \my-vue\vitest.config.ts
+ * @Description:
+ */
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
@@ -8,8 +16,8 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: /@my-vue\/(\w*)/,
-	      replacement: path.resolve(__dirname, "packages") + "/$1/src"
+        find: /@my-vue\/([\w-]*)*/,
+        replacement: path.resolve(__dirname, 'packages') + '/$1/src',
       },
     ],
   },
